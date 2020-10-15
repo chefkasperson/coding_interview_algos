d8274295dc29eba2d8f14ae9e68b016dd256bdb4
chefkasperson <chefkasperson@gmail.com>
2020-10-14 20:43:10 -0700

    added a memoized version of fib

--- fibonacci.js
+++ fibonacci.js
@@ -16,3 +16,19 @@ const fibRecursive = (number) => {
 
     return fibRecursive(number - 1) + fibRecursive(number - 2)
 }
+
+function fibonacciMaster() {
+    let cache = {}
+    return function fib(n) {
+        if (n in cache) {
+            return cache[n]
+        } else {
+            if (n < 2) {
+                return n
+            } else {
+                cache[n] = fib(n-1) + fib(n-2)
+                return cache[n]
+            }
+        }
+    }
+}
